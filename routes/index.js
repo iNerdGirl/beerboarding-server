f73020fe773bff41ec1b2f71bb038f506a6756c3
Autumn Raine <autumnraine@Autumns-MacBook-Pro.local>
2018-09-04 20:12:21 -0600

    😈 added / route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const queries = require('../queries');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({
-    'Title': 'Hello World!  '
-  })
+router.get('/', (req, res, next) => {
+  queries.list('questions')
+    .then(questions => 
+      queries.list('solvers')
+        .then(solvers => 
+          queries.list('questions_solvers')
+            .then(questions_solvers => 
+              queries.list('difficulty')
+                .then(difficulty => 
+                  res.json({
+                    questions: questions,
+                    solvers: solvers,
+                    questions_solvers: questions_solvers,
+                    difficulty: difficulty 
+                  })
+                )
+            )
+        )    
+    )
+    .catch(err => res.status(500).send({ message: err.message }));
 });
 
 module.exports = router;
